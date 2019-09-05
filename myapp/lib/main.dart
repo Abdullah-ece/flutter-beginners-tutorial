93ce55705372ee5f72d9bf0d76b403391662adc8
Abdullah-ece <abdullah.github@gmail.com>
2019-09-05 14:49:15 +0300

    Update main.dart

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Home(), // طريقة ثانية في برمجة الويدجت 
 ));
+// عملية تعريف الويدجت تعرف على انها كلاس وأيضاً سوف ترث الكلاس ال stateless 
+// حتى استطيع تطبيق خاصية الهوت ريلود 
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //تكون الخاصية بخرف صغير في البداية , تكون الويجت بحرف كبير في البداية
+      // تبقى الويجت عبارة عن كلاس 
       appBar: AppBar(
           title: Text('my first app'),
           centerTitle: true,
