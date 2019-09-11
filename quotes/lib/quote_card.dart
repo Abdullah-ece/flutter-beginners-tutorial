09c25bd89ba7371eaed037a6abcc7c0187d7ecb8
Abdullah-ece <abdullah.github@gmail.com>
2019-09-11 20:08:30 +0300

    Update quote_card.dart

--- quotes/lib/quote_card.dart
+++ quotes/lib/quote_card.dart
@@ -3,8 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
 
-  final Quote quote;
-  QuoteCard({ this.quote });
+  final Quote quote; // لأنه متغير غير قابل للتغير بسبب أنه Stateless
+  QuoteCard({ this.quote }); //باني عادى بياخد القيمة تبع المتغير اثناء تعريف الغرض وبحطا بالمتغير الفوقاني
 
   @override
   Widget build(BuildContext context) {
